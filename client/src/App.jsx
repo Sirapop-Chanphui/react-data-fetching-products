6dccdb533b4f9eab10b643bc67ab72c3bb535884
polaris <dinsor1545@gmail.com>
2026-01-13 16:36:05 +0700

    feat: add loading message during request data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,50 +4,55 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]) 
+  const [isWating, setIsWating] = useState(true)
 
   async function requsetReadingData(){
     const response = await axios.get("http://localhost:4001/products")
     setData(response.data.data)
-    
-    // console.log(response)
+    setIsWating(false)
   }
 
   async function handleDelData(id){
+    setIsWating(true)
     const response = await axios.delete(`http://localhost:4001/products/${id}`)
     requsetReadingData()
   }
 
   useEffect(() => {
     requsetReadingData()
+    console.log("wating")
   },[])
 
   return (
     <div className="App">
-      <div className="app-wrapper">
-        <h1 className="app-title">Products</h1>
-      </div>
-      <div className="product-list">
-        {data.map(value => (
-          <div key={value.id} className="product">
-            <div className="product-preview">
-              <img
-                src={value.image}
-                alt="some product"
-                width="350"
-                height="350"
-              />
-            </div>
-            <div className="product-detail">
-              <h1>Product name: {value.name}</h1>
-              <h2>Product price: {value.price} Baht</h2>
-              <p>Product description: {value.description}</p>
-            </div>
+      {isWating? <h1>Loading...</h1> : 
+      <>
+        <div className="app-wrapper">
+          <h1 className="app-title">Products</h1>
+        </div>
+        <div className="product-list">
+          {data.map(value => (
+            <div key={value.id} className="product">
+              <div className="product-preview">
+                <img
+                  src={value.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {value.name}</h1>
+                <h2>Product price: {value.price} Baht</h2>
+                <p>Product description: {value.description}</p>
+              </div>
 
-            <button className="delete-button" onClick={() => handleDelData(value.id)}>x</button>
-          </div>
-        ))}
-
-      </div>
+              <button className="delete-button" onClick={() => handleDelData(value.id)}>x</button>
+            </div>
+          ))}
+        </div>
+      </>
+      }
     </div>
   );
 }
