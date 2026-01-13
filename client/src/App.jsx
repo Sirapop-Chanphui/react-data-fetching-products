ca2a9fc256cf8b59fa943c9865ad1ffa74768406
Sirapop Chanphui <srp073536@gmail.com>
2026-01-13 18:28:06 +0700

    feat: enhance loading status management for product data fetching and deletion in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,26 @@ import axios from "axios";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [loadingStatus, setLoadingStatus] = useState(null);
 
   const getData = async () => {
-    let response = await axios.get("http://localhost:4001/products");
-    setProducts(response.data.data);
+    try {
+      setLoadingStatus("loading");
+      let response = await axios.get("http://localhost:4001/products");
+      setProducts(response.data.data);
+      setLoadingStatus("completed");
+    } catch {
+      setLoadingStatus("error");
+    }
   };
 
   const deleteData = async (id) => {
-    await axios.delete(`http://localhost:4001/products/${id}`);
-    setProducts(products.filter((product) => product.id !== id));
+    try {
+      await axios.delete(`http://localhost:4001/products/${id}`);
+      setProducts(products.filter((product) => product.id !== id));
+    } catch {
+      setLoadingStatus("failed");
+    }
   };
 
   useEffect(() => {
@@ -23,33 +34,37 @@ function App() {
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
-
-        <div className="product-list"></div>
-        {products.map((product) => {
-          return (
-            <div className="product" key={product.id}>
-              <div className="product-preview">
-                <img
-                  src={product.image}
-                  alt="some product"
-                  width="350"
-                  height="350"
-                />
-              </div>
-              <div className="product-detail">
-                <h1>Product name: {product.name}</h1>
-                <h2>Product price:{product.price} Baht</h2>
-                <p>Product description: {product.description}</p>
-              </div>
-              <button
-                className="delete-button"
-                onClick={() => deleteData(product.id)}
-              >
-                x
-              </button>
-            </div>
-          );
-        })}
+        {loadingStatus === "loading" && <h1>Loading...</h1>}
+        {loadingStatus === "failed" && <h1>Fail to load data...</h1>}
+        {loadingStatus === "completed" && (
+          <div className="product-list">
+            {products.map((product) => {
+              return (
+                <div className="product" key={product.id}>
+                  <div className="product-preview">
+                    <img
+                      src={product.image}
+                      alt="some product"
+                      width="350"
+                      height="350"
+                    />
+                  </div>
+                  <div className="product-detail">
+                    <h1>Product name: {product.name}</h1>
+                    <h2>Product price:{product.price} Baht</h2>
+                    <p>Product description: {product.description}</p>
+                  </div>
+                  <button
+                    className="delete-button"
+                    onClick={() => deleteData(product.id)}
+                  >
+                    x
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
